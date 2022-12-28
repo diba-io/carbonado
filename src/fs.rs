81c9bae722db4b12fc66328a5a6ff6a4b01727bd
Hunter Trujillo <cryptoquick@pm.me>
2022-12-28 09:01:37 -0700

    WIP

--- src/fs.rs
+++ src/fs.rs
@@ -2,13 +2,15 @@ use std::{convert::TryFrom, fs::File, io::Read};
 
 use anyhow::{anyhow, Error, Result};
 use bao::Hash;
-use ecies::PublicKey;
+use secp256k1::{schnorr::Signature, KeyPair, Message, PublicKey, Secp256k1, SecretKey};
+use serde::Serialize;
 
 use crate::constants::{Format, MAGICNO};
 
 pub struct Header {
     pub pubkey: PublicKey,
     pub hash: Hash,
+    pub signature: Signature,
     pub format: Format,
     pub verifiable_len: u64,
     pub padding: u16,
@@ -21,6 +23,7 @@ impl TryFrom<File> for Header {
         let mut magic_no = [0_u8; 12];
         let mut pubkey = [0_u8; 33];
         let mut hash = [0_u8; 32];
+        let mut signature = [0_u8; 32];
         let mut format = [0_u8; 2];
         let mut verifiable_len = [0_u8; 8];
         let mut padding = [0_u8; 2];
@@ -29,6 +32,7 @@ impl TryFrom<File> for Header {
         handle.read_exact(&mut magic_no)?;
         handle.read_exact(&mut pubkey)?;
         handle.read_exact(&mut hash)?;
+        handle.read_exact(&mut signature)?;
         handle.read_exact(&mut format)?;
         handle.read_exact(&mut verifiable_len)?;
         handle.read_exact(&mut padding)?;
@@ -38,8 +42,9 @@ impl TryFrom<File> for Header {
                 "File header lacks Carbonado magic number and may not be a proper Carbonado file"
             ));
         }
-        let pubkey = PublicKey::parse_compressed(&pubkey)?;
+        let pubkey = PublicKey::from_slice(&pubkey)?;
         let hash = bao::Hash::try_from(hash)?;
+        let signature = Signature::from_slice(&signature)?;
         let format = u16::from_le_bytes(format);
         let format = Format::try_from(format)?;
         let verifiable_len = u64::from_le_bytes(verifiable_len);
@@ -48,6 +53,7 @@ impl TryFrom<File> for Header {
         Ok(Header {
             pubkey,
             hash,
+            signature,
             format,
             verifiable_len,
             padding,
@@ -57,29 +63,38 @@ impl TryFrom<File> for Header {
 
 impl Header {
     pub fn new(
-        pubkey: PublicKey,
+        sk: SecretKey,
         hash: Hash,
         format: Format,
         verifiable_len: u64,
         padding: u16,
-    ) -> Self {
-        Header {
+    ) -> Result<Self> {
+        let secp = Secp256k1::new();
+        let keypair = KeyPair::from_secret_key(&secp, &sk);
+        let msg = Message::from_slice(hash.as_bytes())?;
+        let signature = keypair.sign_schnorr(msg);
+        let pubkey = PublicKey::from_keypair(&keypair);
+
+        Ok(Header {
             pubkey,
+            signature,
             hash,
             format,
             verifiable_len,
             padding,
-        }
+        })
     }
 
-    /// Adds a total of 100 bytes to
+    /// Creates a total of 100 bytes as a header to be prepended to files.
     pub fn to_vec(&self) -> Vec<u8> {
-        let mut pubkey_bytes = self.pubkey.serialize_compressed().to_vec(); // 33 bytes
+        let mut pubkey_bytes = self.pubkey.serialize().to_vec(); // 33 bytes
         let mut hash_bytes = self.hash.as_bytes().to_vec(); // 32 bytes
+                                                            // let mut signature_bytes: [u8; 32] = self.signature.serialize().unwrap().to_vec(); // 32 bytes
         let mut format_bytes = self.format.bits().to_le_bytes().to_vec(); // 2 bytes
         let mut verifiable_len_bytes = self.verifiable_len.to_le_bytes().to_vec(); // 8 bytes
         let mut padding_bytes = self.padding.to_le_bytes().to_vec(); // 2 bytes
         let mut header = Vec::with_capacity(100);
+
         header.append(&mut MAGICNO.to_vec()); // 12 bytes
         header.append(&mut pubkey_bytes);
         header.append(&mut hash_bytes);
@@ -90,3 +105,7 @@ impl Header {
         header
     }
 }
+
+// fn create_file(header_bytes: &[u8], encoded_bytes: &[u8]) {
+//     todo!();
+// }
--- tests/format.rs
+++ tests/format.rs
@@ -1,1 +1,38 @@
-// TODO
+use anyhow::Result;
+use carbonado::{decode, encode, fs::Header, utils::init_logging};
+use ecies::utils::generate_keypair;
+use log::{debug, info};
+use wasm_bindgen_test::wasm_bindgen_test_configure;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[test]
+fn contract() -> Result<()> {
+    init_logging();
+
+    let input = "Hellow world!".as_bytes();
+    let (privkey, pubkey) = generate_keypair();
+
+    info!("Encoding input: {input:?}...");
+    let (encoded, hash, encode_info) = encode(&pubkey.serialize(), input)?;
+
+    debug!("Encoding Info: {encode_info:#?}");
+    assert_eq!(
+        encoded.len(),
+        encode_info.bytes_verifiable,
+        "Length of encoded bytes matches bytes_verifiable field"
+    );
+
+    info!("Decoding Carbonado bytes");
+    let decoded = decode(
+        &privkey.serialize(),
+        hash.as_bytes(),
+        &encoded,
+        encode_info.padding,
+    )?;
+    assert_eq!(decoded, input, "Decoded output is same as encoded input");
+
+    info!("All good!");
+
+    Ok(())
+}
